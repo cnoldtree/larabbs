dbc2a3cf01547e9a8a60ec0829e7ec8036c83f42
vagrant <vagrant@homestead>
2019-03-05 10:02:02 +0000

    翻译 URL 标示

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -22,8 +22,13 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -39,7 +44,7 @@ class TopicsController extends Controller
         $topic->user_id = \Auth::id();
         $topic->save();
 
-		return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功！');
+		return redirect()->to($topic->link)->with('message', '帖子创建成功！');
 	}
 
 	public function edit(Topic $topic)
@@ -54,7 +59,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->to($topic->link())->with('message', 'Updated successfully.');
 	}
 
 	public function destroy(Topic $topic)
--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,9 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
