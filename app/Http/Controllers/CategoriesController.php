819a46b880dce1295ca1e3c77fe1c9324ee9cfba
vagrant <vagrant@homestead>
2019-03-12 09:53:34 +0000

    bug 修复

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -19,6 +19,11 @@ class TopicObserver
 
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 
     public function saved(Topic $topic)
